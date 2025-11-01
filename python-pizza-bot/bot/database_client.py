4078be1936611e38b2195d20ac064f8c8ce86274
Mikhail Ivanov <misha.ivanov.ig@gmail.com>
2025-11-01 12:35:50 +0300

    feat: upload users database
    Involved function which contains all handlers.
    Made database "users" creation and update.
    Added class EnsureUserExists for check if user has already existed or not.
    
    Signed-off-by: Mikhail Ivanov <misha.ivanov.ig@gmail.com>

--- python-pizza-bot/bot/database_client.py
+++ python-pizza-bot/bot/database_client.py
@@ -14,6 +14,34 @@ def recreate_database() -> None:
             (
                 id INTEGER PRIMARY KEY,
                 payload TEXT NOT NULL
-           )
-       """)
+            )
+            """
+        )
+        connection.execute("DROP TABLE IF EXISTS users")
+        connection.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users
+            (
+                id INTEGER PRIMARY KEY,
+                telegram_id INTEGER NOT NULL UNIQUE,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                state TEXT DEFAULT NULL,
+                order_json TEXT DEFAULT NULL
+            )
+            """,
+        )
     connection.close()
+
+def ensure_user_exists(telegram_id: int) -> None:
+    with sqlite3.connect(os.getenv("SQLITE_DATABASE_PATH")) as connection:
+        with connection:
+            # Check if user exists
+            cursor = connection.execute(
+                "SELECT 1 FROM users WHERE telegram_id = ?", (telegram_id,)
+            )
+
+            # If user doesn't exist, create them
+            if cursor.fetchone() is None:
+                connection.execute(
+                    "INSERT INTO users (telegram_id) VALUES (?)", (telegram_id,)
+                )
--- python-pizza-bot/bot/handlers/__init__.py
+++ python-pizza-bot/bot/handlers/__init__.py
@@ -1,5 +1,6 @@
 from bot.handlers.handler import Handler
 from bot.handlers.update_database_logger import UpdateDatabaseLogger
+from bot.handlers.ensure_user_exists import EnsureUserExists
 from bot.handlers.message_echo import MessageEcho
 from bot.handlers.message_photo_echo import MessagePhotoEcho
 
@@ -7,6 +8,7 @@ from bot.handlers.message_photo_echo import MessagePhotoEcho
 def get_handlers() -> list[Handler]:
     return [
         UpdateDatabaseLogger(),
+        EnsureUserExists(),
         MessageEcho(),
         MessagePhotoEcho(),
     ]
