8be64ea3f04e803af3201e556cf9448b7fdb5e12
Mikhail Ivanov <misha.ivanov.ig@gmail.com>
2025-11-13 02:03:56 +0300

    fix: remove unused input
    Signed-off-by: Mikhail Ivanov <misha.ivanov.ig@gmail.com>

--- python-pizza-bot/bot/__main__.py
+++ python-pizza-bot/bot/__main__.py
@@ -4,7 +4,6 @@ from bot.domain.messenger import Messenger
 from bot.domain.storage import Storage
 from bot.handlers import get_handlers
 from bot.infrastructure.messenger_telegram import MessengerTelegram
-from bot.infrastructure.storage_sqlite import StorageSqlite
 from bot.infrastructure.storage_postgres import StoragePostgres
 
 
